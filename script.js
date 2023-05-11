32ac4e0e3347c8afc15e2ca0cfc5e0c229e0d405
W-Wag <wagner.oliv.m@gmail.com>
2023-05-10 21:29:39 -0300

    Criando base do JS

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ fetch('itens.json')
     .then(json => renderizaItens(json))
     .catch(err => console.error(err));
 
+    const brunch = document.querySelector('.brunch');
+    const details = document.querySelector('.details');
+   
     function renderizaItens(json) {
         for(itens of json){
-            console.log(itens.Nome);
+            const li = document.createElement('li');
+            li.appendChild(details);
+            details.innerHTML = `<h3>${itens.Nome}</h3> <p class="description">${itens.Descricao}<p/>`
+            li.innerHTML += `<strong class="price"> R$${itens.Preco.toFixed(2)} </strong>`;
+
+            brunch.appendChild(li);
+
+
         }
     }
